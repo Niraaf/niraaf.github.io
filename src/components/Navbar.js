501706c1cf60994ce1d4cef5b4051e6f6d89e288
Farin Soriano <72371352+Niraaf@users.noreply.github.com>
2025-03-05 00:44:26 -0800

    Tailwind css, starter home page

---
+++ src/components/Navbar.js
@@ -0,0 +1,43 @@
+'use client';
+
+import { useState } from "react";
+
+export default function Navbar() {
+  const [currentTheme, setCurrentTheme] = useState('light');
+  const themes = ['light', 'dark', 'blue', 'sepia'];
+
+  const changeTheme = () => {
+    const nextTheme = themes[(themes.indexOf(currentTheme) + 1) % themes.length];
+    setCurrentTheme(nextTheme);
+    document.documentElement.setAttribute('data-theme', nextTheme);
+  };
+
+  return (
+    <>
+      {/* Navbar */}
+      <nav className="fixed top-4 left-1/2 transform -translate-x-1/2 p-4 rounded-full text-sm w-[400px] z-50 transition-all duration-300 ease-in-out hover:w-[500px]">
+        <div className="container mx-auto flex items-center justify-between px-4">
+          <ul className="flex space-x-8 w-full justify-center">
+            <li className="ease-in-out transition transform">
+              <a href="/">Home</a>
+            </li>
+            <li className="ease-in-out transition transform">
+              <a href="/about">About</a>
+            </li>
+            <li className="ease-in-out transition transform">
+              <a href="/contact">Contact</a>
+            </li>
+          </ul>
+        </div>
+      </nav>
+
+      {/* Fixed Toggle Button */}
+      <button
+        onClick={changeTheme}
+        className="fixed top-4 right-4 p-4 rounded-full text-sm z-50"
+      >
+        Theme
+      </button>
+    </>
+  );
+}
